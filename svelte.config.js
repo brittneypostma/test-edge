d4120c141ec389874ac9caaf96e5a42a1257e13c
brittneypostma <bdesignedwebdev@gmail.com>
2022-07-21 10:04:39 -0400

    edge true

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter({
+      edge: true
+    }),
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
-};
+    // Override http methods in the Todo forms
+    methodOverride: {
+      allowed: ['PATCH', 'DELETE']
+    }
+  }
+}
 
-export default config;
+export default config
